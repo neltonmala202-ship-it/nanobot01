2363466f3996b4e5a3e104cc154712dafce411de
neltonmala202-ship-it <neltonmala202@gmail.com>
2026-09-23 12:36:56 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dono: ["258858871837"]
+  prefixo: ".",
+  antilink: true,
+  antifoto: false,
+  antivideo: false,
+  antiaudio: false,
+  detector: true,
+  bemVindo: true,
+  nanoAtivo: true,
+  mpesaContas: [],
+  emolaContas: [],
+  concorrentes: [],
+  nanos: {}
+}
